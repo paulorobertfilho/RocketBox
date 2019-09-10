2e75f2ccf2621793cded9f362d148b0be6b5c853
paulorobertfilho <paulorobert.filho@gmail.com>
2019-09-10 14:53:43 -0300

    fixed date-fns to formatDistance instead of distanceInWords

--- frontend/src/pages/Box/index.js
+++ frontend/src/pages/Box/index.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
+import { MdInsertDriveFile } from 'react-icons/md';
+import { formatDistance } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
+import api from '../../services/api';
+import logo from '../../assets/logo.svg';
 import './styles.css';
 
+
 export default class Box extends Component {
+  state = { box: {} }
+  async componentDidMount() {
+    const box = this.props.match.params.id;
+    const response = await api.get(`boxes/${box}`);
+
+    this.setState({ box: response.data })
+  }
   render() {
-    return <div />;
+    return (
+      <div id="box-container">
+        <header>
+          <img src={logo} alt="logo" />
+          <h1>{this.state.box.title}</h1>
+        </header>
+        <ul>
+          {this.state.box.files && this.state.box.files.map(file => (
+            <li>
+              <a className="fileInfo" href={file.url} target="_blank">
+                <MdInsertDriveFile size={24} color="#A5CFFF" />
+                <strong>{file.title}</strong>
+              </a>
+              <span>
+                {formatDistance(new Date(file.createdAt), new Date(),
+                  {
+                    locale: pt,
+                    addSuffix: true
+                  }
+                )}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
